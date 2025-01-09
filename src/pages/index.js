d9d44e3c51ef1f87d848b5cd4b6ec3e64532e5d2
unknown <emoreirasj@gmail.com>
2025-01-09 18:56:29 -0300

    nova apresentação

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 
+
 export default function Home() {
   return (
     <div>
@@ -7,14 +8,12 @@ export default function Home() {
       <div className="iframe-container">
         <iframe
           loading="lazy"
-          src="https://www.canva.com/design/DAGYiy7kEKM/RAX0eoOsnlLjA1W1mmmgdA/view?embed"
-          allowFullScreen
+          src="https://www.canva.com/design/DAGalug2LKg/HSt5I7XYvj4YG1msKpD5hA/view?embed"          allowFullScreen
           allow="fullscreen"
         ></iframe>
       </div>
       <a
-        href="https://www.canva.com/design/DAGYiy7kEKM/RAX0eoOsnlLjA1W1mmmgdA/view?utm_content=DAGYiy7kEKM&utm_campaign=designshare&utm_medium=embeds&utm_source=link"
-        target="_blank"
+href="https://www.canva.com/design/DAGalug2LKg/HSt5I7XYvj4YG1msKpD5hA/view?utm_content=DAGalug2LKg&utm_campaign=designshare&utm_medium=embeds&utm_source=link"        target="_blank"
         rel="noopener noreferrer"
       >
         Projetos
